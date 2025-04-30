c948a5d6f8e9653dac6922930280969499780179
Alejandro Coria <alejandrocoria2009@gmail.com>
2025-04-30 16:50:41 -0300

    updated neoforge reload listener event for 1.21.5

--- neoforge/src/main/java/games/alejandrocoria/imagetitles/client/ClientModEventHandler.java
+++ neoforge/src/main/java/games/alejandrocoria/imagetitles/client/ClientModEventHandler.java
@@ -1,17 +1,21 @@
 package games.alejandrocoria.imagetitles.client;
 
+import games.alejandrocoria.imagetitles.Constants;
 import games.alejandrocoria.imagetitles.ImageTitles;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
-import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
+import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 public class ClientModEventHandler {
-    public static void registerClientReloadListenersEvent(RegisterClientReloadListenersEvent event) {
-        event.registerReloadListener(new SimplePreparableReloadListener<>() {
+    public static void registerClientReloadListenersEvent(AddClientReloadListenersEvent event) {
+        event.addListener(
+                ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "titles_reload_listener"),
+                new SimplePreparableReloadListener<>() {
             @Override
             protected Object prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
                 ImageTitles.loadImageFiles(resourceManager);
