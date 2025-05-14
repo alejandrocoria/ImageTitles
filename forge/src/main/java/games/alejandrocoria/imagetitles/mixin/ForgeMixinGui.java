aabbdbecade81d0aad6c4e0077fd820884fc8d92
Alejandro Coria <alejandrocoria2009@gmail.com>
2025-05-14 17:02:09 -0300

    removed mixinextras and added custom mixin for forge

--- forge/src/main/java/games/alejandrocoria/imagetitles/mixin/ForgeMixinGui.java
+++ forge/src/main/java/games/alejandrocoria/imagetitles/mixin/ForgeMixinGui.java
@@ -2,37 +2,28 @@ package games.alejandrocoria.imagetitles.mixin;
 
 import games.alejandrocoria.imagetitles.ImageTitles;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.network.chat.Component;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
-public class MixinGui {
-    @Inject(at = @At("TAIL"),
-            method = "setTitle")
-    private void setTitle(Component title, CallbackInfo ci) {
-        ImageTitles.setCurrent(title);
-    }
-
-    @ModifyVariable(method = "render",
+@Mixin(ForgeGui.class)
+public class ForgeMixinGui {
+    @ModifyVariable(method = "renderTitle",
                     at = @At(value = "INVOKE",
                             target = "Lnet/minecraft/util/Mth;clamp(III)I",
                             shift = At.Shift.BY,
                             by = 2),
-                    ordinal = 0)
-    private int getKi(int k1) {
+                    ordinal = 2)
+    private int getKi(int opacity) {
         MultiBufferSource.BufferSource buffers = Minecraft.getInstance().renderBuffers().bufferSource();
-        boolean imageRendered = ImageTitles.renderImage(new GuiGraphics(Minecraft.getInstance(), buffers), k1);
+        boolean imageRendered = ImageTitles.renderImage(new GuiGraphics(Minecraft.getInstance(), buffers), opacity);
 
         if (imageRendered) {
             return 0;
         }
-        return k1;
+        return opacity;
     }
 }
