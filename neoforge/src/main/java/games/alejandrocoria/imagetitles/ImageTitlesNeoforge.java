6fb0f5cee73d2e23fd761920ebfd7ed640adab60
Alejandro Coria <alejandrocoria2009@gmail.com>
2025-06-13 15:42:29 -0300

    preventing crashes when loading server-side

--- neoforge/src/main/java/games/alejandrocoria/imagetitles/ImageTitlesNeoforge.java
+++ neoforge/src/main/java/games/alejandrocoria/imagetitles/ImageTitlesNeoforge.java
@@ -1,17 +1,15 @@
 package games.alejandrocoria.imagetitles;
 
 import games.alejandrocoria.imagetitles.client.ClientModEventHandler;
-import games.alejandrocoria.imagetitles.client.ClientNeoForgeEventHandler;
+import games.alejandrocoria.imagetitles.client.ImageTitlesClientNeoForge;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod(Constants.MOD_ID)
 public class ImageTitlesNeoforge {
     public ImageTitlesNeoforge(IEventBus eventBus) {
-        ImageTitles.init();
-
         eventBus.addListener(ClientModEventHandler::registerClientReloadListenersEvent);
-        NeoForge.EVENT_BUS.addListener(ClientNeoForgeEventHandler::entityJoinLevelEvent);
+        eventBus.addListener((FMLClientSetupEvent event) -> ImageTitlesClientNeoForge.clientSetup(event, eventBus));
     }
 }
